278d7c0d71eb9f5a2903ff04498e8158080113d2
ANGEL-HR00 <herreroangel11@gmail.com>
2024-05-27 19:23:30 +0200

    Regresion lineal datos medicos

--- src/app.py
+++ src/app.py
@@ -1,4 +1,27 @@
-from utils import db_connect
-engine = db_connect()
-
 # your code here
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+
+train_data = pd.read_csv("train_sel.csv")
+test_data = pd.read_csv("src/test_sel.csv")
+
+
+X_train = train_data.drop(["charges"], axis = 1)
+y_train = train_data["charges"]
+X_test = test_data.drop(["charges"], axis = 1)
+y_test = test_data["charges"]
+
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+print(f"Intercepto (a): {model.intercept_}")
+print(f"Coeficientes (b1, b2): {model.coef_}")
+
+
+y_pred = model.predict(X_test)
+y_pred
+print(f"MSE: {mean_squared_error(y_test, y_pred)}")
+print(f"R2 Score: {r2_score(y_test, y_pred)}")
